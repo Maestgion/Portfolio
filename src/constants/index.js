ce3f24870d58f1c8f13dff84b32972264aec8bfa
Abhishek Dubey <7abhishek1410@gmail.com>
2023-04-11 11:26:23 +0530

    updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,7 +46,7 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Beginner AR Developer",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Content Writer",
       icon: creator,
     },
   ];
