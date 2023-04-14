afc3cffd613c10b083cc68083ea3b454af7ba08f
Abhishek Dubey <7abhishek1410@gmail.com>
2023-04-14 09:53:19 +0530

    about-section-completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import {Tilt} from 'react-tilt'
-import {styles} from "../style"
-import {services} from "../constants"
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../style";
+import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
-import {fadeIn, textVariant} from "../utils/motion"
+import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
@@ -37,34 +38,29 @@ const ServiceCard = ({ index, title, icon }) => (
 const About = () => {
   return (
     <>
-    {/* heading */}
-    <motion.div 
-    variants={textVariant()}  
-    >
-      <p className={`${styles.sectionSubText} `}>Introduction</p>
-      <h2 className={`${styles.sectionHeadText} `}>
-        Overview
-      </h2>
-    </motion.div>
+     {/* heading */}
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-    {/* bio */}
-    <motion.p
-    variants={fadeIn("", "", 0.1, 1)}
-    className=' mt-4 text-secondary max-w-3xl tetx-[17px] leading-[30px]'
-    >
-    As a full stack developer, I am passionate about exploring new technologies and staying up-to-date with the latest trends. With a keen eye for detail, I strive to build dynamic and user-friendly applications that provide seamless experiences for users. As a beginner AR developer, I enjoy experimenting with AR technologies and creating immersive experiences. Additionally, I am a creative content writer with a knack for crafting compelling copy and engaging content. Through my diverse skill set, I am driven to create innovative solutions that make a positive impact in the world.
-    </motion.p>
+       {/* bio */}
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+         As a full stack developer, I am passionate about exploring new technologies and staying up-to-date with the latest trends. With a keen eye for detail, I strive to build dynamic and user-friendly applications that provide seamless experiences for users. As a beginner AR developer, I enjoy experimenting with AR technologies and creating immersive experiences. Additionally, I am a creative content writer with a knack for crafting compelling copy and engaging content. Through my diverse skill set, I am driven to create innovative solutions that make a positive impact in the world.
+      </motion.p>
 
-      {/* service - cards */}
-
+     {/* service - cards */}
+     
       <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
     </>
-    
-  )
-}
+  );
+};
 
 export default SectionWrapper(About, "about");
--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
-
-import { styles } from "../styles";
+import { styles } from "../style";
 import { staggerContainer } from "../utils/motion";
 
 const SectionWrapper = (Component, idName) =>
