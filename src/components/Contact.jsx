4c1d2fe84c8f10b8e01eaf4372cf6e8c769c6f27
Abhishek Dubey <7abhishek1410@gmail.com>
2023-04-14 18:29:19 +0530

    contact-section-done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,9 +15,48 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setForm({...form, [name]:value})
+  };
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true)
+
+    console.log( import.meta.env.VITE_APP_EMAILJS_SERVICE_ID)
+
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_KEY,
+      {
+        from_name : form.name,
+        to_name: "Abhishek Dubey",
+        from_email : form.email,
+        to_email : "7abhishek1410@gmail.com",
+        message : form.message, 
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+
+    ).then(
+      () => {
+        setLoading(false);
+        alert("Thank you!! I will get back to you as soon as possible.");
+
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        });
+      },
+      (error) => {
+        setLoading(false);
+        console.error(error);
+
+        alert("Aw snap! something went wrong. Please try again.");
+      }
+    );
+  };
 
   return (
     <>
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
@@ -31,7 +30,7 @@ const Stars = (props) => {
 const StarsCanvas = () => {
   return (
     <div className='w-full h-auto absolute inset-0 z-[-1]'>
-      <Canvas  camera={{ position: [0, 0, 1] }}>
+      <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
